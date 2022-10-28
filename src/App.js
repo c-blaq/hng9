8b204274f08bac1e4ac4201db82bcb460c6803a3
Fathi <babayejuadesina@gmail.com>
2022-10-28 19:24:51 +0100

    social icons added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import ProfileSection from "./first-stage/ProfileSection/ProfileSection";
 import LinkSection from "./first-stage/LinkSection/LinkSection";
 import Footer from "./components/Footer/Footer";
+import Socials from "./first-stage/Socials";
 
 function App() {
   return (
     <div>
       <ProfileSection />
       <LinkSection />
+      <Socials />
       <Footer />
     </div>
   );
---
+++ src/first-stage/Socials.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./LinkSection/index.css";
+
+const Socials = () => {
+  return (
+    <div id="social-icon-wrapper">
+      <img src="/assets/images/slack.jpg" alt="slack logo" />
+      <img src="/assets/images/github.png" alt="github logo" />
+    </div>
+  );
+};
+
+export default Socials;
