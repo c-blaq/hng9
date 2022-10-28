bb183788214ee59c125e42c5fb2e8d67f5b394da
Fathi <babayejuadesina@gmail.com>
2022-10-28 19:32:25 +0100

    simple layout

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import ProfileSection from "./first-stage/ProfileSection/ProfileSection";
 import LinkSection from "./first-stage/LinkSection/LinkSection";
 import Footer from "./components/Footer/Footer";
 import Socials from "./first-stage/Socials";
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <ProfileSection />
-      <LinkSection />
-      <Socials />
+    <div className="App">
+      <div id="first-stage">
+        <ProfileSection />
+        <LinkSection />
+        <Socials />
+      </div>
       <Footer />
     </div>
   );
