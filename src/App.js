46e29cbf9d13413bf0898a5d85d2ddab600c0775
Fathi <babayejuadesina@gmail.com>
2022-10-28 19:10:09 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ProfileSection from "./first-stage/ProfileSection/ProfileSection";
 import LinkSection from "./first-stage/LinkSection/LinkSection";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div>
       <ProfileSection />
       <LinkSection />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h2>Zuri.Internship</h2>
+      <span>HNG Internship 9 Frontend Task</span>
+      <img src="/assets/images/I4G.png" alt="I4G logo" />
+    </footer>
+  );
+};
+
+export default Footer;
