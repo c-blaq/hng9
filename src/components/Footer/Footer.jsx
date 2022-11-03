cf1788eba9ab876929caf3f7438f860389782e26
Fathi <babayejuadesina@gmail.com>
2022-11-03 17:31:18 +0100

    style:footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,8 +4,10 @@ import "./footer.css";
 const Footer = () => {
   return (
     <footer>
-      <h2>Zuri.Internship</h2>
-      <span>HNG Internship 9 Frontend Task</span>
+      <h2 className="text-xl sm:text-2xl font-medium">Zuri.Internship</h2>
+      <span className="text-sm sm:text-base">
+        HNG Internship 9 Frontend Task
+      </span>
       <img src="/assets/images/I4G.png" alt="I4G logo" />
     </footer>
   );
