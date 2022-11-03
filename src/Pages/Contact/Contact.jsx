39ff6f295801afa1096290d65beb2da6136652e1
Fathi <babayejuadesina@gmail.com>
2022-11-03 17:12:49 +0100

    style: responsiveness desktop view

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -4,37 +4,39 @@ import PageLayout from "../../Layout/layout";
 const Contact = () => {
   return (
     <PageLayout>
-      <div className="text-left">
+      <div className="text-left max-w-[720px] mx-auto">
         <h1 className="font-semibold text-3xl text-gray-900">Contact Me</h1>
         <p className="text-lg mt-4 text-gray-600">
           Hi there, contact me to ask me about anything you in mind.
         </p>
         <form className="mt-12 mb-20 flex flex-col gap-6">
-          <label
-            htmlFor="first_name"
-            className="text-gray-700 text-sm font-medium"
-          >
-            First name
-            <input
-              className="w-full mt-1 border border-gray-300 py-2 px-3 rounded-lg"
-              type="text"
-              id="first_name"
-              placeholder="Enter your first name"
-            />
-          </label>
+          <div className="m-0 text-left flex flex-col gap-6 sm:flex-row">
+            <label
+              htmlFor="first_name"
+              className="text-gray-700 sm:flex-1 text-sm font-medium"
+            >
+              First name
+              <input
+                className="w-full mt-1 border border-gray-300 py-2 px-3 rounded-lg"
+                type="text"
+                id="first_name"
+                placeholder="Enter your first name"
+              />
+            </label>
 
-          <label
-            htmlFor="last_name"
-            className="text-gray-700 text-sm font-medium"
-          >
-            Last name
-            <input
-              type="text"
-              id="last_name"
-              placeholder="Enter your last name"
-              className="w-full mt-1 border border-gray-300 py-2 px-3 rounded-lg"
-            />
-          </label>
+            <label
+              htmlFor="last_name"
+              className="text-gray-700 text-sm font-medium flex-1"
+            >
+              Last name
+              <input
+                type="text"
+                id="last_name"
+                placeholder="Enter your last name"
+                className="w-full  mt-1 border border-gray-300 py-2 px-3 rounded-lg"
+              />
+            </label>
+          </div>
 
           <label htmlFor="email" className="text-gray-700 text-sm font-medium">
             Email
@@ -53,8 +55,8 @@ const Contact = () => {
             Message
             <textarea
               id="message"
-              placeholder=" Send me a message and I'll reply you as soon as possible"
-              className="border border-gray-300 mt-1 w-full h-[132px]"
+              placeholder="Send me a message and I'll reply you as soon as possible"
+              className="border p-3 border-gray-300 mt-1 w-full h-[132px]"
             ></textarea>
           </label>
 
